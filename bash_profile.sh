e10c45b3fe1b518ae5a65b8cc09abbdb5801352a
kalle <kasaar2@gmail.com>
2017-09-05 22:03:49 -0700

    wip

--- bash_profile.sh
+++ bash_profile.sh
@@ -5,10 +5,13 @@ alias p="pwd"
 alias dp="cd ~/Desktop"
 alias sb="source ~/.bash_profile"
 alias eb="vim ~/.bash_profile"
+alias el="vim ~/.bash_local"
 
+alias ga='git add'
+alias gd='git diff'
 alias gs="git status"
 
-if [ -z ~/.bash_local ]
+if [ -e ~/.bash_local ]
 then
 	source ~/.bash_local
 fi
